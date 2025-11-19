eac7c366b918ccfca0f063a81c4a4232cfe167ed
wirtrax <ngrbuster@gmail.com>
2025-11-19 16:01:07 +0300

    'base'

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './app/taskList/integration/store';
+import App from './components/App';
 
 const container = document.getElementById('root');
 const root = createRoot(container as HTMLElement);
-
-root.render(<h1>todo app</h1>);
+root.render(
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
